b5f233618003d776e26d473e69f170ad1c581c81
lvcf <lv.canfeng@geely.com>
2018-10-18 20:19:42 +0800

    增加备注使查找更加方便

--- blockchain_go/block.go
+++ blockchain_go/block.go
@@ -4,6 +4,7 @@ import (
 	"crypto/sha256"
 	"fmt"
 	"strconv"
+	"time"
 )
 
 //简易区块链的数据结构
@@ -14,6 +15,9 @@ type Block struct {
 	Hash []byte
 }
 
+type Blockchain struct {
+	blocks []*Block
+}
 func SetHash(b *Block) {
 	h := sha256.New()
 	timestamp := []byte(strconv.FormatInt(b.Timestamp,10))
@@ -25,7 +29,24 @@ func SetHash(b *Block) {
 	b.Hash = h.Sum(nil)
 }
 
+/**
+生成一个区块
+ */
+func NewBlock( data string, preBlockhash []byte ) *Block{
+	block := &Block{time.Now().Unix(),[]byte(data),preBlockhash,[]byte{}}
+	SetHash(block)
+	return block
+}
+
+func (bc *Blockchain) AddBlock(data string) {
+	prevBlock := bc.blocks[len(bc.blocks)-1]
+	newBlock := NewBlock(data, prevBlock.Hash)
+	bc.blocks = append(bc.blocks, newBlock)
+}
+
+
 func main() {
 	h := sha256.New()
 	fmt.Println(h)
+
 }
